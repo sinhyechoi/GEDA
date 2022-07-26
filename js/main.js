5732e066e3712bae39d724cdf46eb72079146d8d
sinhyechoi <107171378+sinhyechoi@users.noreply.github.com>
2022-07-26 10:45:21 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ new fullpage('#fullpage', {
     normalScrollElements: '#s2',
 });
 
+
+//햄버거 버튼 클릭
+$('.btn_total').click(function(){
+    $('.total_menu_z').addClass('active');
+});
+
+
+//메뉴 팝업 닫기 버튼
+$('.btn_totalClose').click(function(){
+    $('.total_menu_z').removeClass('active')
+});
+
+
+var myScroll = new IScroll('.total_menu',{
+    mouseWheel:true,
+    //scrollbars:ture
+});
+
+$('.totalMitem').click(function(){
+    if($(this).hasClass('active')){//현재 active가 적용된 상태일 때,
+       $(this).removeClass('active');
+} else {
+    $('.totalMitem').removeClass('active');
+    $(this).addClass('active');
+}
+});
